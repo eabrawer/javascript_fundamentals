b2d68e0001c4cbf6373662b78196d0eb056be60f
Eric Brawer <ericbrawer@gmail.com>
2013-09-24 09:56:21 -0400

    first commit

---
+++ js_fundamentals/variables.js
@@ -0,0 +1,49 @@
+// Exercises: Variables
+
+// EXERCISE: The Fortune Teller
+
+var nb_kids= "3";
+var partner = "Laura";
+var place = "Paris";
+var job = "Politician";
+
+var future = "you will be have " + nb_kids + " kids, your wife will be named" +
+			partner + "you will live in" + place + "and be a" + jon + ".";
+
+// EXERCISE: The Age Calculator
+
+var current_year = 2013
+var birth_year = 1990
+var age = "They are either" + (current_year - birth_year) " or " 
+	(current_year - birth_year - 1) + '.'
+console.log(age)
+
+// EXERCISE: The Lifetime Supply Calculator
+
+var current_age = 22;
+var max_age = 80;
+var amount = 1;
+var life = (max_age - current_age) * 365 * amount;
+console.log(life)
+
+
+// EXERCISE: The Geometrizer
+
+var radius = 10;
+var pi = 3.14;
+var diameter = 20;
+var circumference = pi * diameter;
+
+console.log(circumference)
+
+// EXERCISE: The Temperature Converter
+
+var celcius = 1;
+var fahrenheit_conversion = 1.8;
+var fahrenheit_base = 32
+var fahrenheit = celcius * fahrenheit_conversion + fahrenheit_base
+var to_celcius = (fahrenheit - fahrenheit_base) / fahrenheit_conversion
+console.log(fahrenheit)
+console.log(to_celcius)
+
+
