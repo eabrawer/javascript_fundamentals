2fd05ec76c481737ad308ba0967faa9e7fd2d1f3
Eric Brawer <ericbrawer@gmail.com>
2013-09-26 21:59:18 -0400

    EXERCISE: Change Parent

--- jquery_fundamentals/jquery_exercises.js
+++ jquery_fundamentals/jquery_exercises.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
 	$(".target").eq(1).css("background-color", "red")
 	$("button").attr("disabled", "disable")
 	$("inbox").removeAttr("checked")
+	$("#child").appendTo("#parent2")
 });
 
 		
